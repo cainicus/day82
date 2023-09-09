4a2c7e1d6223fd99d633f02da4daf94bb1977554
Cain Haigh-Simpson <cain.hs@icloud.com>
2023-09-09 18:50:03 +1000

    improve Morse code translation program.

--- main.py
+++ main.py
@@ -3,17 +3,31 @@ import pandas as pd
 data = pd.read_csv('data/morse.csv')
 morse_dict = {row.letter: row.morse for (index, row) in data.iterrows()}
 
-# print(morse_dict)
+print("\nWelcome to Morse code translator. \n ")
 
-print("Enter the phrase to translate into Morse code.")
-phrase = input("NOTE: Only enter letters, spaces and numbers. No punctuation.\n").upper()
 
-translation = []
+def translate():
+    print("Enter the phrase to translate into Morse code.")
+    phrase = input("NOTE: Any special characters undefined by Mose code will be included as is.\n")
 
-for char in phrase:
-    if char == " ":
-        translation.append("/")
+    translation = []
+
+    for char in phrase.upper():
+        if char == " ":
+            translation.append("/")
+        elif char not in morse_dict.keys():
+            translation.append(char)
+        else:
+            translation.append(morse_dict[char])
+
+    print(f"\nThat translates to\n{' '.join(translation)}\nin Morse code.\n")
+
+    repeat = input("Would you like to translate another phrase? Y/n: \n")
+    if repeat[0].upper() == "Y":
+        translate()
     else:
-        translation.append(morse_dict[char])
+        print("Thank you. Have a nice day.")
+        return
 
-print(' '.join(translation))
+translate()
+
